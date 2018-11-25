7774eba8cb63cc4188e625087f98987f796e3798
Hunter Trammell <hunterttrammell@gmail.com>
2018-11-25 16:14:21 -0600

    made changes to router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <navbar></navbar>
-    <jumbotron></jumbotron>
-    <homebody></homebody>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <a class="navbar-brand text-white" href="/">Expense.</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="username" placeholder="Username" aria-label="Username">
+          <input class="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password">
+          <router-link id="login" class="btn btn-outline-success my-2 my-sm-0" to="/dashboard">Sign In</router-link>
+        </form>
+      </div>
+    </nav>
+
+    <router-view/>
   </div>
 </template>
 
 <script>
-import navbar from './components/navbar.vue'
-import jumbotron from './components/jumbotron.vue'
-import homebody from './components/homebody.vue'
 
 export default {
-  name: 'app',
-  components: {
-    navbar,
-    jumbotron,
-    homebody
-  }
+  name: 'app'
 }
 </script>
 
@@ -28,6 +32,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #163840;
+}
+.btn-outline-success{
+  color: #85F2A0;
+}
+.btn-outline-success:hover {
+  color: #163840;
+  background-color: #85F2A0;
 }
 
 </style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <jumbotron></jumbotron>
+    <homebody></homebody>
+  </div>
+</template>
+
+<script>
+import jumbotron from './jumbotron.vue'
+import homebody from './homebody.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    jumbotron,
+    homebody
+  }
+}
+</script>
