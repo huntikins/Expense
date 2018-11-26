fb0c783bd66ae08bdd25ce044e879ebb8632ed6b
Hunter Trammell <hunterttrammell@gmail.com>
2018-11-25 21:34:14 -0600

    updated dash

---
+++ client/src/components/dashboard_frame.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div id="greeting" class="col-12 pt-2">
+                <h2>Lets take a look at your wallet {{ userName }}</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <!--insert translog div here-->
+            </div>
+            <div class="col">
+                <!--insert upcoming bills div here-->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <!--insert receiptdiv here-->
+            </div>
+            <div class="col">
+                <!--insert budgetdiv here-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        userName: "Elon Musk"
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
