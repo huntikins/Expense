30459b5842bcf40c6e19152b241328d43b31f693
Douglas Boyce <douglasboyce07@gmail.com>
2018-11-27 20:16:13 -0600

    add budget model

--- models/category.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  const category = sequelize.define('category', {
-    categoryId: DataTypes.INTEGER,
-    category_name: DataTypes.STRING,
-    budget_amount: DataTypes.INTEGER,
-  });
-  return category;
-};
