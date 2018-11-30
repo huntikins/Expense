63957bcba2f32d9b423165b3b0482f8ed1be3e16
RyanJF <ryanjfreeman77@gmail.com>
2018-11-29 22:47:42 -0600

    trying to figure out merge issues

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,21 +1,21 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
-const transactionsController = require('../../controllgit -helpers/transactions');
+const transactionsController = require('../../controllers/transactions');
 require('../../config/server');
 require('../../controllers/passport');
 
-router.post('/',
-    require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
-    transactionsController.create
-);
+// router.post('/',
+//     require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
+//     transactionsController.create
+// );
 
-router.get('/',
-    require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
-    transactionsController.findAll
-);
+// router.get('/',
+//     require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
+//     transactionsController.findAll
+// );
 
-router.put('/',
-    require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
-    transactionsController.update
-);
+// router.put('/',
+//     require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
+//     transactionsController.update
+// );
 
 module.exports = router;
