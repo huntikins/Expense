00b8489c5f26bc60f385b0acba9a213aef76c171
djknit <djknit@gmail.com>
2018-11-29 20:33:29 -0600

    budget backend

--- routes/api/budget.js
+++ routes/api/budget.js
@@ -3,20 +3,19 @@ const budgetController = require('../../controllers/budget');
 require('../../config/server');
 require('../../controllers/passport');
 
-router.post('/', (req, res) => {
-    require('../../config/server').passport.authenticate('local', { failureRedirect: '/api/authentication/fail', failureFlash: true }),
-    function(req, res) {
-        if (!req.body.categoryId || !req.body.amount) return res.status(400);
-        budgetController.create({
-            categoryId: req.body.categoryId,
-            amount: req.body.amount,
-            userId: req.user.id
-        },
-        result => {
-            console.log(result);
-            res.send(result);
-        });
-    }
-});
+router.post('/',
+    require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
+    budgetController.create
+);
+
+router.get('/',
+    require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
+    budgetController.findAll
+);
+
+router.put('/',
+    require('connect-ensure-login').ensureLoggedIn('/api/authentication/fail'),
+    budgetController.create
+);
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 const noteRoutes = require('./examples');
 const authRoutes = require('./authentication');
+const budgetRoutes = require('./budget');
 router.use('/examples', noteRoutes);
 router.use('/authentication', authRoutes);
+router.use('/budget', budgetRoutes);
 
 module.exports = router;
