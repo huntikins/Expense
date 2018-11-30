5a43e76d9b8e19c433e9b8defb548481f024dab7
RyanJF <ryanjfreeman77@gmail.com>
2018-11-29 23:16:30 -0600

    confirm that image upload is working.

--- controllers/awsController.js
+++ controllers/awsController.js
@@ -4,6 +4,8 @@ module.exports = {
     post: (req, res) => {
         try {
             imageUri = s3.upload(req.body.image);
+            console.log(imageUri);
+            // res.send(imageUri);
         }
         catch (err) {
             res.status(500).send(err.message)
--- services/awsService.js
+++ services/awsService.js
@@ -12,9 +12,10 @@ const s3Bucket = new aws.S3({ params: { Bucket: keys.S3_Bucket } });
 
 module.exports = {
     upload: (image) => {
-        buf = new Buffer(image.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+        buf = new Buffer(image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+        let time = Date.now().toString();
         var data = {
-            Key: Date.now(),
+            Key: `receipt-snap-${time}`,
             Body: buf,
             ContentEncoding: 'base64',
             ContentType: 'image/png'
@@ -22,7 +23,6 @@ module.exports = {
 
         s3Bucket.upload(data, function (err, data) {
             if (err) {
-                console.log(err);
                 console.log('Error uploading data: ', data);
                 throw new Error(err);//res.status(500).send(err.message);
             } else {
